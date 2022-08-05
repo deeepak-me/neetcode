0eafbce5cb2a59c20c39b640c1d3b594e4418dbf
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-08-05 13:06:55 +0530

    feat: js solution 3

---
+++ Contains Duplicate/jsSol3.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+// sort mathod
+
+var containsDuplicate = function(nums) {
+    nums.sort((a, b) => { return b - a });
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i - 1] === nums[i]) return true;
+    }
+    return false
+};
